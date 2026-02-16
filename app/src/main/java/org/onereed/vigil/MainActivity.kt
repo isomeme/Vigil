c53bc2b95c0e4a654c24e34d3e17c9651d849e9e
Catherine Berry <cdberry@gmail.com>
2026-02-15 21:57:04 -0800

    Add main button image. Implement skeleton of home screen.

--- app/src/main/java/org/onereed/vigil/MainActivity.kt
+++ app/src/main/java/org/onereed/vigil/MainActivity.kt
@@ -4,16 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import org.onereed.vigil.ui.theme.VigilTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,22 +17,9 @@ class MainActivity : ComponentActivity() {
     setContent {
       VigilTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-          Greeting(name = "Android", modifier = Modifier.padding(innerPadding))
+          HomeScreen(modifier = Modifier.padding(innerPadding))
         }
       }
     }
   }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-  Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-    Text(text = "Hello $name!", color = MaterialTheme.colorScheme.primary)
-  }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-  VigilTheme { Greeting("Android") }
 }
